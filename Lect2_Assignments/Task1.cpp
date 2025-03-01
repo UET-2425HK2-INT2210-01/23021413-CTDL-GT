16858cfbc87f44eccb08b7c11078fa5e3c1ead0b
Khánh Đào <186800803+Khanh59-sts@users.noreply.github.com>
2025-03-01 11:14:36 +0700

    Update Task1.cpp

--- Lect2_Assignments/Task1.cpp
+++ Lect2_Assignments/Task1.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    int n;
+    cin >> n; 
+    vector<int> A(n); 
+    for (int i = 0; i < n; i++) {
+        cin >> A[i];
+    }
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) { 
+            if (A[i] == A[j]) {
+                count++;
+            }
+        }
+    }
+    cout << count << endl; 
+    return 0;
+}
 
