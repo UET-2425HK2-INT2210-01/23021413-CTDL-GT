e4cfce9ff06208fc5ffe33f40f5baa31819aefff
Khánh Đào <186800803+Khanh59-sts@users.noreply.github.com>
2025-03-04 11:35:11 +0700

    Update Task6.cpp

--- Lect3_Assignments/Task6.cpp
+++ Lect3_Assignments/Task6.cpp
@@ -1,51 +1,46 @@
 // Stack Node Structure: 
-struct Node {
+struct Node 
     int data;
-    Node* next;
-};
+    Node* next
 
 // Push Operation: Time Complexity: O(1)
-function push(Node*& top, int value) {
-    Node* newNode = new Node();
-    newNode->data = value;
-    newNode->next = top;  // Point to the current top node
-    top = newNode;  // Update the top to the new node
-}
+function push(Node*& top, int value) 
+    Node* newNode = new Node()
+    newNode->data = value
+    newNode->next = top  // Point to the current top node
+    top = newNode  // Update the top to the new node
+
 
 // Pop Operation: Time Complexity: O(1)
-function pop(Node*& top) {
-    if (top == nullptr) {
-        throw "Stack Underflow";  // Error if the stack is empty
-    }
+function pop(Node*& top) 
+    if (top == nullptr) 
+        throw "Stack Underflow"  // Error if the stack is empty
     Node* temp = top;
-    int value = top->data;
-    top = top->next;  // Update top to the next node
-    delete temp;  // Free the memory of the popped node
-    return value;
-}
+    int value = top->data
+    top = top->next  // Update top to the next node
+    delete temp  // Free the memory of the popped node
+    return value
+
 
 // Peek Operation: Time Complexity: O(1)
-function peek(Node* top) {
-    if (top == nullptr) {
-        throw "Stack Underflow";  // Error if the stack is empty
-    }
-    return top->data;  // Return the value of the top node
-}
+function peek(Node* top) 
+    if (top == nullptr) 
+        throw "Stack Underflow"  // Error if the stack is empty
+    return top->data  // Return the value of the top node
 
 // IsEmpty Operation: Time Complexity: O(1)
-function isEmpty(Node* top) {
-    return top == nullptr;  // Return true if the stack is empty
-}
+function isEmpty(Node* top) 
+    return top == nullptr  // Return true if the stack is empty
 
 // Size Operation: Time Complexity: O(n)
-function size(Node* top) {
+function size(Node* top) 
     int count = 0;
-    Node* current = top;
-    while (current != nullptr) {
-        count++;
-        current = current->next;
-    }
+    Node* current = top
+    while (current != nullptr) 
+        count++
+        current = current->next
+
     return count;  // Return the number of elements in the stack
-}
+
 
 
