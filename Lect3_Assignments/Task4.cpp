d9cf5b3f7c4719b413b384b3fc9bff3eaf227a6f
Khánh Đào <186800803+Khanh59-sts@users.noreply.github.com>
2025-03-08 10:59:21 +0700

    Update Task4.cpp

--- Lect3_Assignments/Task4.cpp
+++ Lect3_Assignments/Task4.cpp
@@ -2,42 +2,42 @@
 // Time Complexity: O(1)
 // Space Complexity: O(1)
 function enqueue(queue, data):
-    node = new Node(data)       // Create a new node with data
-    if queue.rear == null:      // If the queue is empty
-        queue.front = node      // Both front and rear point to the new node
+    node = new Node(data)      
+    if queue.rear == null:      
+        queue.front = node     
         queue.rear = node
     else:
-        queue.rear.next = node  // Link the new node to the last node in the queue
-        queue.rear = node       // Update the rear pointer to the new node
+        queue.rear.next = node  
+        queue.rear = node      
     end if
 
 // Dequeue Operation
 // Time Complexity: O(1)
 // Space Complexity: O(1)
 function dequeue(queue):
-    if queue.front == null:         // If the queue is empty
-        return "Queue is empty"     // Return error or empty queue indication
+    if queue.front == null:         
+        return "Queue is empty"    
     else:
-        front_node = queue.front    // Get the front node
-        queue.front = queue.front.next // Move the front pointer to the next node
-        if queue.front == null:     // If the queue is now empty
-            queue.rear = null       // Reset rear to null
+        front_node = queue.front   
+        queue.front = queue.front.next 
+        if queue.front == null:   
+            queue.rear = null       
         end if
-        return front_node.data      // Return the data of the removed node
+        return front_node.data     
     end if
 
 // Peek Operation
 // Time Complexity: O(1)
 // Space Complexity: O(1) 
 function peek(queue):
-    if queue.front == null:         // If the queue is empty
-        return "Queue is empty"     // Return error or empty queue indication
+    if queue.front == null:        
+        return "Queue is empty"     
     else:
-        return queue.front.data     // Return the data of the front node
+        return queue.front.data     
     end if
 
 // Is Empty Operation
 // Time Complexity: O(1)
 // Space Complexity: O(1)
 function isEmpty(queue):
-    return queue.front == null     // Return true if front is null, indicating the queue is empty
+    return queue.front == null     
