8710aaa1edb32042a221ac619a7f0202d69369e9
Khánh Đào <186800803+Khanh59-sts@users.noreply.github.com>
2025-03-08 10:59:59 +0700

    Update Task6.cpp

--- Lect3_Assignments/Task6.cpp
+++ Lect3_Assignments/Task6.cpp
@@ -7,30 +7,30 @@ struct Node
 function push(Node*& top, int value) 
     Node* newNode = new Node()
     newNode->data = value
-    newNode->next = top  // Point to the current top node
-    top = newNode  // Update the top to the new node
+    newNode->next = top  
+    top = newNode  
 
 
 // Pop Operation: Time Complexity: O(1)
 function pop(Node*& top) 
     if (top == nullptr) 
-        throw "Stack Underflow"  // Error if the stack is empty
+        throw "Stack Underflow"  
     Node* temp = top;
     int value = top->data
-    top = top->next  // Update top to the next node
-    delete temp  // Free the memory of the popped node
+    top = top->next  
+    delete temp  
     return value
 
 
 // Peek Operation: Time Complexity: O(1)
 function peek(Node* top) 
     if (top == nullptr) 
-        throw "Stack Underflow"  // Error if the stack is empty
-    return top->data  // Return the value of the top node
+        throw "Stack Underflow"  
+    return top->data  
 
 // IsEmpty Operation: Time Complexity: O(1)
 function isEmpty(Node* top) 
-    return top == nullptr  // Return true if the stack is empty
+    return top == nullptr  
 
 // Size Operation: Time Complexity: O(n)
 function size(Node* top) 
@@ -40,7 +40,7 @@ function size(Node* top)
         count++
         current = current->next
 
-    return count;  // Return the number of elements in the stack
+    return count;  
 
 
 
