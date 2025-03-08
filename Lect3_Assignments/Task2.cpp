6622598986a5da7e72eb6d724edd5515e0f844f1
Khánh Đào <186800803+Khanh59-sts@users.noreply.github.com>
2025-03-08 10:53:26 +0700

    Update Task2.cpp

--- Lect3_Assignments/Task2.cpp
+++ Lect3_Assignments/Task2.cpp
@@ -1,17 +1,17 @@
 // Algorithm 1: [Time complexity O(n)]
 Function calculateTwoPowerIterative(n):
-    result = 1  // Start with 1
+    result = 1  
     for i = 1 to n:
-        result = result * 2  // Multiply by 2 each time
+        result = result * 2  
     return result
   
 // Algorithm 2: [Time complexity O(log n)]
 Function calculateTwoPowerEfficient(n):
-    result = 1  // Start with 1
-    base = 2    // Start with base 2
+    result = 1  
+    base = 2    
     while n > 0:
         if n is odd:
-            result = result * base  // Multiply result by base if n is odd
-        base = base * base   // Square the base
-        n = n // 2   // Divide n by 2 (integer division)
+            result = result * base  
+        base = base * base   
+        n = n 
     return result
