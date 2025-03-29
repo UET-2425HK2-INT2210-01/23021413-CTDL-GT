4776ceb5bd13fa49d58e59cb4c1d8938e238bb25
Khánh Đào <186800803+Khanh59-sts@users.noreply.github.com>
2025-03-29 11:16:00 +0700

    Update Task1a.cpp

--- Lect6_Assignments/Task1a.cpp
+++ Lect6_Assignments/Task1a.cpp
@@ -1,3 +1,35 @@
+      //   2
+      //  / \
+      // 1   10
+      //    /  \
+      //   6    13
+      //  / \    \
+      // 3   8    20
+      //    /
+      //   7
+// Thêm số 1:
+// 1 nhỏ hơn 2 → đi sang nhánh trái của 2
+// Thêm số 10:
+// 10 lớn hơn 2 → đi sang nhánh phải của 2
+// Thêm số 6:
+// 6 nhỏ hơn 10 (node hiện tại) nhưng lớn hơn 2
+// → đi sang nhánh trái của 10
+// Thêm số 3:
+// 3 nhỏ hơn 6 nhưng lớn hơn 2
+// → đi sang nhánh trái của 6
+// Thêm số 8:
+// 8 lớn hơn 6 nhưng nhỏ hơn 10
+// → đi sang nhánh phải của 6
+// Thêm số 7:
+// 7 nhỏ hơn 8 nhưng lớn hơn 6
+// → đi sang nhánh trái của 8
+// Thêm số 13:
+// 13 lớn hơn 10
+// → đi sang nhánh phải của 10
+// Thêm số 20:
+// 20 lớn hơn 13
+// → đi sang nhánh phải của 13
+
 #include <iostream>
 using namespace std;
 struct Node {
