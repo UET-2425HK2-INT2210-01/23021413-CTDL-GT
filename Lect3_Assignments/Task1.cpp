ce29b3b8c2bbf9f67e21d2920a7ad99a1888bcf4
Khánh Đào <186800803+Khanh59-sts@users.noreply.github.com>
2025-04-11 20:52:44 +0700

    Update Task1.cpp

--- Lect3_Assignments/Task1.cpp
+++ Lect3_Assignments/Task1.cpp
@@ -1,3 +1,8 @@
+1. Sort the following functions in the ascending order of Big O notation:
+4nlogn + 2n                 |      2^10       |       2^logn
+3n + 100logn                |      4n         |         2^n
+n^2 + 10n                   |      n^3        |        nlogn 
+
 1. 2^10 → O(1) 
 2. 4n → O(n) 
 3. 3n + 100logn → O(n) 
