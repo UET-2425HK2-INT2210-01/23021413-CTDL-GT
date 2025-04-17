683862e32223bbb6eec64f46e31adf9d1daee8e0
Khánh Đào <186800803+Khanh59-sts@users.noreply.github.com>
2025-04-17 16:32:11 +0700

    Update Task1.cpp

--- Lect7.0_Assignments_Recursion/Task1.cpp
+++ Lect7.0_Assignments_Recursion/Task1.cpp
@@ -2,17 +2,17 @@
 using namespace std;
 
 // Hàm đệ quy để tính tổng từ 1 đến n
-int sum(int n) {
+int deQuy(int n) {
     // Điều kiện dừng khi n = 0
     if (n == 0) {
         return 0;
     }
     // Gọi đệ quy: sum(n) = n + sum(n - 1)
-    return n + sum(n - 1);
+    return n + deQuy(n - 1);
 }
 
 int main() {
     int n = 5;
-    cout << sum(n) << endl;
+    cout << deQuy(n) << endl;
     return 0;
 }
