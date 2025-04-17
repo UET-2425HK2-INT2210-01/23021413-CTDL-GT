907983d912ac5b635f4dc7cc023279eda1971e02
Khánh Đào <186800803+Khanh59-sts@users.noreply.github.com>
2025-04-17 16:32:51 +0700

    Update Task2.cpp

--- Lect7.0_Assignments_Recursion/Task2.cpp
+++ Lect7.0_Assignments_Recursion/Task2.cpp
@@ -2,17 +2,16 @@
 using namespace std;
 
 // Hàm đệ quy tính giai thừa của n
-int factorial(int n) {
+int giaiThua(int n) {
     // Điều kiện dừng khi n = 0 hoặc n = 1
     if (n == 0 || n == 1) {
         return 1;
     }
-    // Gọi đệ quy: factorial(n) = n * factorial(n - 1)
-    return n * factorial(n - 1);
+    return n * giaiThua(n - 1);
 }
 
 int main() {
     int n = 5;
-    cout << factorial(n) << endl;
+    cout << giaiThua(n) << endl;
     return 0;
 }
